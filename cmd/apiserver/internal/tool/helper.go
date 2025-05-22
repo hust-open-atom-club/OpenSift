1adef19d8ea35f1ac43533b9b780a072a9960052
Cheng Ziqiu <chengziqiu@hust.edu.cn>
2025-05-22 18:40:37 +0800

    chore: make error output red

--- cmd/apiserver/internal/tool/helper.go
+++ cmd/apiserver/internal/tool/helper.go
@@ -88,6 +88,26 @@ func RunExternalCommand(args []string, env []string, in io.Reader, out io.Writer
 
 type SimpleToolRunner func(args map[string]any, in io.Reader, out io.Writer, err io.Writer, kill chan int) error
 
+type RedWriter struct {
+	w io.Writer
+}
+
+func (r *RedWriter) Write(p []byte) (n int, err error) {
+	b := []byte("\033[31m")
+	b = append(b, p...)
+	b = append(b, []byte("\033[0m")...)
+	rn, err := r.w.Write(b)
+	inN := rn - len("\033[31m")
+	if inN < 0 {
+		n = 0
+	} else if inN >= len(p) {
+		n = len(p)
+	} else {
+		n = inN
+	}
+	return
+}
+
 func CanioalizeWrapper(f SimpleToolRunner) ToolRunner {
 	return func(args map[string]any, in io.Reader, out io.Writer, kill chan int, resize chan ResizeArg) (int, error) {
 		ptmx, tty, err := pty.Open()
@@ -125,7 +145,9 @@ func CanioalizeWrapper(f SimpleToolRunner) ToolRunner {
 			}
 		}()
 
-		err = f(args, tty, tty, tty, kill)
+		err = f(args, tty, tty, &RedWriter{
+			w: tty,
+		}, kill)
 
 		// there is no need to cosume the resize channel
 		if err != nil {
