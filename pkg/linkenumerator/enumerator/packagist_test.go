971b5c9e120b7879c27c7a183442576992b8efa6
xiongzhen1 <U202212073@hust.edu.cn>
2025-08-06 10:03:04 +0800

    Create_packagist_test.go

---
+++ pkg/linkenumerator/enumerator/packagist_test.go
@@ -0,0 +1,26 @@
+package enumerator
+
+import (
+	"testing"
+
+	"github.com/HUSTSecLab/OpenSift/pkg/linkenumerator/writer"
+)
+
+// Test PackagistEnumerator.Enumerate with MemoryWriter
+func TestPackagistEnumerate(t *testing.T) {
+	e := NewPackagistEnumerator(5)
+	testWriter := writer.NewTestWriter()
+	e.SetWriter(testWriter)
+	err := e.Enumerate()
+	if err != nil {
+		t.Error(err)
+	}
+	// Print output content
+	for i, line := range testWriter.Lines {
+		t.Logf("Package %d: %s", i+1, line)
+	}
+	// Assert output count
+	if len(testWriter.Lines) != 30 {
+		t.Errorf("Expected 30 lines, got %d", len(testWriter.Lines))
+	}
+}
